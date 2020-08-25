2a79de677c77bfb934266dfecdefca2ba4063f93
John Dutton <johnmdutton1@gmail.com>
2020-08-25 11:12:12 -0500

    first version of if else practice

---
+++ IfElsePractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IfElsePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            Console.WriteLine("Give me a number between 1 and 1000");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Thats too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Thats too high");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+        }
+    }
+}
